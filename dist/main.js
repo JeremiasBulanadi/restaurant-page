4b929fc0655e3b7384cb6f08e92eafac3785a227
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-01-21 22:09:27 +0800

    Fixed choice not selecting

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,22 @@
+// TODO:
+
 // initial page load code
-homeChoice = document.getElementById("home-choice") ;
+let homeChoice = document.getElementById("home-choice") ;
 homeChoice.classList.add("active"); // home page is selected by default
 
-bottomMenu = document.getElementById("bottom-menu");
-for (let element in bottomMenu.childNodes) {
-    console.log(element);
+let bottomMenu = document.getElementById("bottom-menu");
+let choices = Array.from(bottomMenu.getElementsByClassName("choice"));
+choices.forEach(choice => choice.onclick = "selectPage(event)");
+for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener('click',selectPage);
 }
 
 // functions
 function selectPage (e) {
-    e.currentTarget.className = "choice active" ;
-    
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].className = "choice";
+    }
+    target = e.currentTarget;
+    target.className = target.className === "choice" ? "choice active" : "choice" ;
 
-}
-
-// renders the page selected
-function renderPage () {
-    
 }
